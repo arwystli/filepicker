96001696f25f9bc27c193767d071d929d80edda2
Bob Hutchinson <hutch@midwales.net>
2010-11-29 21:38:38 +0000

    initial D7 commit

--- filepicker.js
+++ filepicker.js
@@ -9,11 +9,34 @@
  * Adapted by Bob Hutchinson for filepicker upload form
 */
 
-( function ($) {
+/**
+ * Hide the node form and show the busy div
+*/
+function filepicker_upload_progress_hide() {
+  $('#filepicker-upload-form').hide();
+  $("#sending").show();
+  $("#filepicker_upload_progress_cancel_link").click(filepicker_upload_progress_show);
+}
+
+function filepicker_upload_progress_show() {
+  $('#filepicker-upload-form').show();
+  $("#sending").hide();
+
+  // "reload" the form
+  window.location = window.location;
+}
+
+function filepicker_upload_progress_hide_timeout() {
+  var delay = Drupal.settings["filepicker_upload_progress"]["delay"];
+  setTimeout('filepicker_upload_progress_hide()', delay*1000);
+
+}
+
 
 /**
  * Hide the node form and show the busy div
 */
+/*
   Drupal.filepicker_upload_progress_hide_timeout = function() {
     var delay = Drupal.settings.filepicker_upload_progress.delay;
     setTimeout('filepicker_upload_progress_hide', delay*1000);
@@ -32,72 +55,76 @@
     // "reload" the form
     window.location = window.location;
   }
+*/
 
-  Drupal.behaviors.filepicker = function() {
-    // Attaches the upload behaviour to the filepicker upload form.
-    $('#filepicker-upload-form').submit(Drupal.filepicker_upload_progress_hide_timeout);
+(function ($) {
+  Drupal.behaviors.filepicker = {
+    attach: function(context) {
 
-    // form enhancement
-    if ($("#edit-filepicker-quota-byrole").attr('checked')) {
-      $("#wrap_filepicker_quota_role").show();
-    }
-    else {
-      $("#wrap_filepicker_quota_role").hide();
-    }
-    $("#edit-filepicker-quota-byrole").change(function() {
+      // Attaches the upload behaviour to the filepicker upload form.
+      $('#filepicker-upload-form').submit(filepicker_upload_progress_hide_timeout);
+
+      // form enhancement
       if ($("#edit-filepicker-quota-byrole").attr('checked')) {
         $("#wrap_filepicker_quota_role").show();
       }
       else {
         $("#wrap_filepicker_quota_role").hide();
       }
-    });
+      $("#edit-filepicker-quota-byrole").change(function() {
+        if ($("#edit-filepicker-quota-byrole").attr('checked')) {
+          $("#wrap_filepicker_quota_role").show();
+        }
+        else {
+          $("#wrap_filepicker_quota_role").hide();
+        }
+      });
 
-    if ($("#edit-filepicker-import-enabled").attr('checked')) {
-      $("#wrap-filepicker-import").show();
-    }
-    else {
-      $("#wrap-filepicker-import").hide();
-    }
-    $("#edit-filepicker-import-enabled").change(function() {
       if ($("#edit-filepicker-import-enabled").attr('checked')) {
         $("#wrap-filepicker-import").show();
       }
       else {
         $("#wrap-filepicker-import").hide();
       }
-    });
+      $("#edit-filepicker-import-enabled").change(function() {
+        if ($("#edit-filepicker-import-enabled").attr('checked')) {
+          $("#wrap-filepicker-import").show();
+        }
+        else {
+          $("#wrap-filepicker-import").hide();
+        }
+      });
 
 
-    if ($("#edit-filepicker-upload-progress-enabled").attr('checked')) {
-      $("#wrap-filepicker-upload-progress").show();
-    }
-    else {
-      $("#wrap-filepicker-upload-progress").hide();
-    }
-    $("#edit-filepicker-upload-progress-enabled").change(function() {
       if ($("#edit-filepicker-upload-progress-enabled").attr('checked')) {
         $("#wrap-filepicker-upload-progress").show();
       }
       else {
         $("#wrap-filepicker-upload-progress").hide();
       }
-    });
+      $("#edit-filepicker-upload-progress-enabled").change(function() {
+        if ($("#edit-filepicker-upload-progress-enabled").attr('checked')) {
+          $("#wrap-filepicker-upload-progress").show();
+        }
+        else {
+          $("#wrap-filepicker-upload-progress").hide();
+        }
+      });
 
-    if ($("#edit-group-public").attr('checked')) {
-      $("#wrap-group-public-roles").show();
-    }
-    else {
-      $("#wrap-group-public-roles").hide();
-    }
-    $("#edit-group-public").change(function() {
       if ($("#edit-group-public").attr('checked')) {
         $("#wrap-group-public-roles").show();
       }
       else {
         $("#wrap-group-public-roles").hide();
       }
-    });
+      $("#edit-group-public").change(function() {
+        if ($("#edit-group-public").attr('checked')) {
+          $("#wrap-group-public-roles").show();
+        }
+        else {
+          $("#wrap-group-public-roles").hide();
+        }
+      });
+    }
   };
-
 })(jQuery);

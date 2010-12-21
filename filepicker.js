6c61fc23835a262434c52cd7655bc30874a07095
Bob Hutchinson <hutch@midwales.net>
2010-12-21 23:41:57 +0000

    added PECL uploadprogress support

--- filepicker.js
+++ filepicker.js
@@ -1,43 +1,14 @@
 // $Id$
 /**
  * @file
- * Javascript functions for progress bar and form enhancement
+ * Javascript functions for admin form enhancement
  *
- * By Bob Hutchinson for filepicker upload form
 */
 
-(function ($) {
-
-/**
- * Hide the node form and show the busy div
-*/
-
-  Drupal.filepicker_upload_progress_hide = function() {
-    $('#filepicker-upload-form').hide();
-    $("#sending").show();
-    $("#filepicker_upload_progress_cancel_link").click(Drupal.filepicker_upload_progress_show);
-  }
-
-  Drupal.filepicker_upload_progress_hide_timeout = function() {
-    var delay = Drupal.settings.filepicker_upload_progress.delay;
-    setTimeout(Drupal.filepicker_upload_progress_hide, delay*1000);
-  }
-
-  Drupal.filepicker_upload_progress_show = function() {
-    $('#filepicker-upload-form').show();
-    $("#sending").hide();
-
-    // "reload" the form
-    window.location = window.location;
-  }
-
+( function ($) {
   Drupal.behaviors.filepicker = {
     attach: function(context) {
 
-      // Attaches the upload behaviour to the filepicker upload form.
-      $('#filepicker-upload-form', context).submit(Drupal.filepicker_upload_progress_hide_timeout);
-
-      // form enhancement
       $("#edit-filepicker-quota-byrole", context).change(function() {
         if ($(this).attr('checked')) {
           $("#wrap-filepicker-quota-role", context).show();
@@ -73,7 +44,6 @@
           $("#wrap-filepicker-groups", context).hide();
         }
       });
-
     }
   };
 })(jQuery);
